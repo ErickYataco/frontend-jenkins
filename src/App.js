e01d62f1b98133433efb79924587f922840128e5
erick yataco <erickyataco@ericks-MacBook-Pro.local>
2020-01-26 21:58:25 -0500

    version 6

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React V
+          Learn React VI
         </a>
       </header>
     </div>
