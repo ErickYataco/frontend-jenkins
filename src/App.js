717b613a9d6653983cb3eed2e133028e9f05671b
erick yataco <erickyataco@ericks-MacBook-Pro.local>
2020-02-04 09:02:03 -0500

    update version

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React IX
+          Learn React X
         </a>
       </header>
     </div>
