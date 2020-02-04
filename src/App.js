eb56baeaae96e0e23f44c4ef8fc103532eefdaf3
erick yataco <erickyataco@ericks-MacBook-Pro.local>
2020-02-04 12:36:08 -0500

    change version

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React X
+          Learn React XI
         </a>
       </header>
     </div>
